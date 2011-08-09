f9e5c6cf36c90bd158a01ffffafdb8cb2ba5d20e
Jun Komatsu <jun@uniba.jp>
2011-08-09 17:31:03 +0900

    unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,11 +13,12 @@ role :db, "sakura", :primary => true
 
 set :rails_env, :production
 set :unicorn_binary, "/usr/bin/unicorn"
+set :unicorn_config, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -p 8081 -E #{rails_env} -D"
+    run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -p 8081 -c #{unicorn_config} -E #{rails_env} -D"
   end
  
   task :stop, :roles => :app, :except => { :no_release => true } do
---
+++ config/unicorn.rb
@@ -0,0 +1,22 @@
+$default_env = "production" # デフォルトRailsEnv
+$unicorn_user = "deploy" # slaveの実行ユーザ
+$unicorn_group = "deploy" # slaveの実行グループ
+
+$dev_processes = 4 # dev環境用子プロセス数
+$prod_processes = 16 # 本番環境用子プロセス数
+
+$timeout = 75 # タイムアウト秒数。タイムアウトしたslaveは再起動される
+
+# String => UNIX domain socket / FixNum => TCP socket
+#$listen = "/home/bps/tmp/unicorn.sock"
+$listen = 5000
+
+# ---- end of config ----
+
+# Main Config for Unicorn
+rails_env = ENV['RAILS_ENV'] || $default_env
+worker_processes (rails_env == 'production' ? $prod_processes : $dev_processes)
+preload_app true
+timeout $timeout
+listen $listen, :backlog => 2048
+pid "${RAILS_APP_ROOT}/log/unicorn.pid"
