1749c16ef09510f821c99e69a635d1e326887e7f
Jun Komatsu <jun@uniba.jp>
2011-08-09 17:10:12 +0900

    deploy start

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,3 +10,17 @@ set :use_sudo, false
 role :app, "sakura"
 role :web, "sakura"
 role :db, "sakura", :primary => true
+
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && BUNDLE_GEMFILE=#{current_path}/Gemfile bundle exec unicorn_rails -c #{current_path}/config/unicorn.rb -E production -D"
+  end
+ 
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "kill -KILL -s QUIT `cat #{shared_path}/pids/unicorn.pid`"
+  end
+ 
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "kill -KILL -s USR2 `cat #{shared_path}/pids/unicorn.pid`"
+  end
+end
