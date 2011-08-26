df02a3bec299702e15d99d8e4fba7922714db82d
Jun Komatsu <jun@uniba.jp>
2011-08-27 01:25:42 +0900

    items seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#coding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,11 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Item.create([
+  {:type => 1, :name => 'item1', :data => 'freedata1'},
+  {:type => 1, :name => 'item2', :data => 'freedata2'},
+  {:type => 1, :name => 'item3', :data => 'freedata3'},
+  {:type => 1, :name => 'item4', :data => 'freedata4'}
+]);
+
