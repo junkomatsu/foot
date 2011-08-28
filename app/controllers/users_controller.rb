b19a041fa2b9db63f4ae3c31bdb0ed6a3979345a
Jun Komatsu <jun@uniba.jp>
2011-08-28 20:38:36 +0900

    log testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
     conditions[:status] = params[:status] if params[:status]
     order = params[:order]
     @users = User.where(conditions).order(order);
-    logger.info request.headers['Accept']
+    logger.info('log test')
+    logger.info(request.headers['Accept'])
     respond_with @users
   end
 
